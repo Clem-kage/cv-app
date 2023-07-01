5fcbabf8f407246296e3eebb35aebf172c7122b1
clemk <clemkfr@gmail.com>
2023-07-01 20:17:52 +0200

    first commit

---
+++ js/about.29039a57.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcuriculum_vue2"]=self["webpackChunkcuriculum_vue2"]||[]).push([[443],{1864:function(u,n,t){t.r(n),t.d(n,{default:function(){return i}});var e=function(){var u=this;u._self._c;return u._m(0)},s=[function(){var u=this,n=u._self._c;return n("div",{staticClass:"about"},[n("h1",[u._v("This is an about page")])])}],c=t(1001),l={},r=(0,c.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.29039a57.js.map
